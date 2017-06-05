cb96ca44b803ae4234e0a5f824bad17cb267d2b2
Danny Aw <kokwah07@hotmail.com>
2017-06-05 17:33:14 +0800

    Added Description and remove spacing error

--- src/triangle.c
+++ src/triangle.c
@@ -1,40 +1,22 @@
 #include "triangle.h"
 
-/*void identifyLengths(Triangle triangle,int a, int b, int c){
-	int max=a;
-	triangle->longest=a;
-	triangle->short1=b;
-	triangle->short2=c;
-	
-	if(b>max){
-		max=b;
-		triangle->longest=b;
-		triangle->short1=a;
-		triangle->short2=c;
-	}
-	else if(c>max){
-		max=c;
-		triangle->longest=c;
-		triangle->short1=a;
-		triangle->short2=b;
-	}
-}*/
-
+/*
+** Sorting which is the longest side of the triangle,
+** short1, and short2. Return 0 if invalid.
+*/
 char *sortSidesOfTriangle(int a,int b,int c,int *s1,int *s2,int *l){
 	if(a>b&&a>c){
-	*l=a;
-	*s1=b;
-	*s2=c;
+		*l=a;
+		*s1=b;
+		*s2=c;
 	}
 	
 	else if (b>a&&b>c){
-	
 		*l=b;
 		*s1=a;
 		*s2=c;
 	}
 	else if(c>a&&c>b){
-	
 		*l=c;
 		*s1=a;
 		*s2=b;
@@ -43,9 +25,13 @@ char *sortSidesOfTriangle(int a,int b,int c,int *s1,int *s2,int *l){
 		return 0;
 }
 
+/*	
+**	
+*/
 char *validityOfTriangle(int a,int b,int c){
 	int s1,s2,l;
 	sortSidesOfTriangle(a,b,c,&s1,&s2,&l);
+	
 	if(s1+s2>l)
 		return "valid";
 	else
@@ -54,8 +40,10 @@ char *validityOfTriangle(int a,int b,int c){
 
 char *TypeOfTriangle(int a,int b,int c){
 	int s1,s2,l;
+	
 	sortSidesOfTriangle(a,b,c,&s1,&s2,&l);
 	validityOfTriangle(a,b,c);
+	
 	if(s1*s1+s2*s2<l*l)
 		return "accute triangle";
 	else if(s1*s1+s2*s2>l*l)
--- test/test_triangle.c
+++ test/test_triangle.c
@@ -19,43 +19,37 @@ void test_sortSidesOfTriangle(){
   printf("longest:%d\n",l);
   
   TEST_ASSERT_EQUAL(7,l);
- 
-  
    }
 
 void test_ValidityOfTriangle_3_plus_4_longer_than_5_expect_valid(){
   int a,b,c,s1,s2,l;
   
-  char * result=validityOfTriangle(4,5,3);
+  char* result=validityOfTriangle(4,5,3);
 
   TEST_ASSERT_EQUAL_STRING("valid",result);
-  
    }
    
    
 void test_TypeOfTriangle_A2_plus_B2_equal_C2_expect_rightAngle(){
   int a,b,c,s1,s2,l;
   
-  char * result1=TypeOfTriangle(3,4,5);
+  char* result1=TypeOfTriangle(3,4,5);
 
   TEST_ASSERT_EQUAL_STRING("right angle",result1);
-  
    }
    
  void test_TypeOfTriangle_A2_plus_B2_larger_than_C2_expect_obtuseAngle(){
   int a,b,c,s1,s2,l;
   
-  char * result1=TypeOfTriangle(5,7,8);
+  char* result1=TypeOfTriangle(5,7,8);
 
-  TEST_ASSERT_EQUAL_STRING("obtuse triangle",result1);
-  
+  TEST_ASSERT_EQUAL_STRING("obtuse triangle",result1); 
    }
    
 void test_TypeOfTriangle_A2_plus_B2_smaller_than_C2_expect_accuteAngle(){
   int a,b,c,s1,s2,l;
   
-  char * result1=TypeOfTriangle(4,3,6);
+  char* result1=TypeOfTriangle(4,3,6);
 
-  TEST_ASSERT_EQUAL_STRING("accute triangle",result1);
-  
+  TEST_ASSERT_EQUAL_STRING("accute triangle",result1);  
    }
